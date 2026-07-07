e6d05ed78985d895a7dfbd986050e2008ba8c68c
PavanSurisetti <pavan1898235@gmail.com>
2026-07-07 20:18:08 +0530

    Delete Command in SQLAlchemy

---
+++ Delete_in_SQLAlchemy.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine,String,Integer,Float,Column
+from sqlalchemy.orm import declarative_base,sessionmaker
+Base=declarative_base()
+class Student(Base):
+    __tablename__='StudentDetails2'
+    sid=Column(Integer,primary_key=True)
+    sname=Column(String)
+    sage=Column(Integer)
+    sper=Column(Float)
+engine=create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)#this will creates the table in the Database
+s1=Student(sid=1,sname='Ganesha',sage=19,sper=99.9)
+s2=Student(sid=2,sname='Satya',sage=20,sper=98.9)
+s3=Student(sid=3,sname='Pavan',sage=20,sper=95.43)
+s4=Student(sid=4,sname='Surya',sage=21,sper=96.98)
+s5=Student(sid=5,sname='Charles',sage=23,sper=81.9)
+Session=sessionmaker(bind=engine)
+session=Session()
+session.add_all([s1,s2,s3,s4,s5])
+session.add_all([s2,s3,s5])
+session.commit()
+print('DataBase Contains Before Delete:')
+print('Sid \t Sname \t Sage \t sper')
+data=session.query(Student).all()
+for i in data:
+    print(i.sid,'\t',i.sname,'\t',i.sage,'\t',i.sper)
+#deleting the data
+temp=session.query(Student).filter(Student.sid==5).first()
+if temp:
+    session.delete(temp)
+    session.commit()
+    print('DataBase Contains After Delete:')
+    print('Sid \t Sname \t Sage \t sper')
+    data=session.query(Student).all()
+    for i in data:
+        print(i.sid,'\t',i.sname,'\t',i.sage,'\t',i.sper)
+else:
+    print('record not found')
+#multiple delete
+session.query(Student).filter(Student.sage==20).delete()
+session.commit()
+print('DataBase Contains After Multiple Deletes:')
+print('Sid \t Sname \t Sage \t sper')
+data=session.query(Student).all()
+for i in data:
+    print(i.sid,'\t',i.sname,'\t',i.sage,'\t',i.sper)
